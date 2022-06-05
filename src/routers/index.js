6dec554223943322a5cfa5a81b781711efc0cf9a
cl0ud <148cl0ud@gmail.com>
2022-06-06 01:02:32 +0900

    get 추가

--- src/routers/index.js
+++ src/routers/index.js
@@ -27,6 +27,9 @@ const router = new Router();
 router
   .get('/Admin', dbFind.getAdmin)
   .get('/Product', dbFind.getProduct)
+  .get('/Progress', dbFind.getProgress)
+  .get('/Target', dbFind.getTarget)
+  .get('/Total', dbFind.getTotal)
 
   .post('/Admin', dbInsert.insertAdmin);
 
