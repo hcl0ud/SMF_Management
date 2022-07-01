ea39fe5149c37742a0ace6b89fd10bc757e0fc35
cl0ud <148cl0ud@gmail.com>
2022-07-01 20:44:11 +0900

    구조 리팩토링 취소...

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'SMF_MNG',
-      script: 'nodemon --watch src/ src/index.js',
+      script: 'nodemon --watch src/ src/db.controller.js',
       instances: 0,
       exec_mode: 'fork',
     },
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,9 +1,8 @@
 const Router = require('@koa/router');
 const router = new Router();
 
-const db = require('../database');
-const Account = require('../database/Account.controller');
-const mqtt = require('../MQTT');
+const db = require('./db.controller');
+const MQTT = require('../MQTT');
 
 router
   // Management System CRUD
@@ -15,16 +14,16 @@ router
   .post('/Target/Update', db.insertTarget)
 
   // Account CRUD
-  .post('/Admin/Login', Account.loginAdmin)
-  .post('/Admin/Register', Account.registerAdmin)
+  .post('/Admin/Login', db.loginAdmin)
+  .post('/Admin/Register', db.registerAdmin)
 
-  .post('/User/Login', Account.loginUser)
-  .post('/User/Register', Account.registerUser)
+  .post('/User/Login', db.loginUser)
+  .post('/User/Register', db.registerUser)
 
-  .post('/User2/Login', Account.loginUser2)
-  .post('/User2/Register', Account.registerUser2);
+  .post('/User2/Login', db.loginUser2)
+  .post('/User2/Register', db.registerUser2);
 
-mqtt.Con(); // MQTT Connect
-mqtt.Sub(); // MQTT Subscribe
+MQTT.Con(); // MQTT Connect
+MQTT.Sub(); // MQTT Subscribe
 
 module.exports = router;
