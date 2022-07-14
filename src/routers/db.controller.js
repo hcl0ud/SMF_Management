5ec27ce7e16a8eff58e4cf4c98b3d187174b32a9
cl0ud <148cl0ud@gmail.com>
2022-07-14 16:31:53 +0900

    getBoardList logic fix6

--- src/routers/db.controller.js
+++ src/routers/db.controller.js
@@ -161,7 +161,7 @@ exports.getBoardDetail = async (ctx) => {
 };
 
 exports.insertBoard = async (ctx) => {
-  console.log(JSON.stringify(ctx));
+  console.log(ctx.request.body);
   await Board.insertOne(ctx);
   ctx.body = { message: '게시글 작성 완료' };
 };
